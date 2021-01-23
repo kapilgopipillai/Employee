7fa070e75ecbfd46c4c0a10e898ac2fb6d8d5c20
Kapilgopipillai <kapilgp93@gmail.com>
2021-01-24 00:33:45 +0530

    Adding Client side

---
+++ Server/Employee_Server/Employee.Model/ListQueryResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee.Model
+{
+    public class ListQueryResult<TEntity>
+    {
+        public ListQueryResult(IEnumerable<TEntity> payload, int totalCount)
+        {
+            Payload = payload;
+            TotalCount = totalCount;
+        }
+
+        public readonly IEnumerable<TEntity> Payload;
+
+        public readonly int TotalCount;
+    }
+}
---
+++ Server/Employee_Server/Employee_Server/Controllers/EmployeeBaseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee.Api.Controllers
+{
+    public class EmployeeBaseController : ControllerBase
+    {
+        protected IActionResult Updated() => StatusCode(StatusCodes.Status202Accepted);
+
+        protected IActionResult Deleted() => StatusCode(StatusCodes.Status202Accepted);
+
+        protected IActionResult Created() => StatusCode(StatusCodes.Status201Created);
+
+        protected IActionResult Created(Guid newId) => StatusCode(StatusCodes.Status201Created, newId);
+
+        protected IActionResult SendMail(string newId) => StatusCode(StatusCodes.Status200OK, newId);
+    }
+}
